63146ab14663bcb33d8247c680fa98fb79182c84
jellyb3e <itsjuliamari@gmail.com>
2025-10-06 23:43:03 +0000

    added click counter and counter text (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+let counter = 0;
+
 document.body.innerHTML = `
+  <h1>meese explosion factory.</h1>
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-  <button class="moots">🫎</button>
+  <button id="moots">🫎</button>
+  <div>meese exploded: <span id="counter">0</span></div>
 `;
+
+// click handler
+const button = document.getElementById("moots")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  counter++;
+  counterElement.innerHTML = `${counter}`;
+});
