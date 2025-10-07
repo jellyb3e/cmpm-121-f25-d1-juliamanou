0794c2fb1d3521aa73432ab6d8234c6f1a623212
jellyb3e <itsjuliamari@gmail.com>
2025-10-07 00:03:20 +0000

    implemented auto-click (step 3 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,27 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-let counter = 0;
+let counter: number = 0;
 
 document.body.innerHTML = `
   <h1>meese explosion factory.</h1>
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
   <button id="moots">🫎</button>
-  <div>meese exploded: <span id="counter">0</span></div>
+  <div><span id="counter">0</span> meese obliterated</div>
 `;
+// consider: blown to smithereens
 
 // click handler
 const button = document.getElementById("moots")!;
 const counterElement = document.getElementById("counter")!;
 
 button.addEventListener("click", () => {
+  click();
+});
+
+setInterval(click, 1000);
+
+function click() {
   counter++;
   counterElement.innerHTML = `${counter}`;
-});
+}
