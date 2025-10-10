5d08ee24df18123ae5290c3f1a6a0b604b39fe73
jellyb3e <itsjuliamari@gmail.com>
2025-10-10 23:01:57 +0000

    added 2 buy buttons and status displays (step 6 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,79 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 let counter: number = 0; // click counter
 let lastTime: number = 0; // used for auto-click calculation
 let autoClicks: number = 0; // 0 autoclicks to start
-let upgradeCost: number = 10; // cost to upgrade autoclicker
 
+const costA: number = 10; // cost to upgrade autoclicker a
+const costB: number = 100; // cost to upgrade autoclicker b
+const costC: number = 1000; // cost to upgrade autoclicker c
+
+const upsA: number = 0.1; // units per second
+const upsB: number = 2;
+const upsC: number = 50;
+
+let purchasedA: number = 0;
+let purchasedB: number = 0;
+let purchasedC: number = 0;
+
+// auto-moose-obliterator
 document.body.innerHTML = `
   <h1>meese explosion factory.</h1>
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
   <button id="moots">🫎</button>
-  <div><span id="counter">0</span> meese blown to smithereens</div>
+  <div class="content-text"><span id="counter">0</span> meese blown to smithereens</div>
+  <div class="label-text">obliter-rate-ion: <span id="growthRate">0</span> meese/sec</div>
   <br>
-  <button id="buyButton" disabled>buy auto-moose-obliterator</button>
-  <div>cost: <span id="upgradeCost">10</span> doubloons</div>
+  <button id="buyButtonA" disabled>upgradeA (+0.1 meese/sec)</button>
+  <div class="label-text">cost: <span id="upgradeCostA">10</span> doubloons</div>
+  <div class="label-text">purchased: <span id="purchasedA">0</span> units</div>
+  <br>
+  <button id="buyButtonB" disabled>upgradeB (+2.0 meese/sec)</button>
+  <div class="label-text">cost: <span id="upgradeCostB">100</span> doubloons</div>
+  <div class="label-text">purchased: <span id="purchasedB">0</span> units</div>
+  <br>
+  <button id="buyButtonC" disabled>upgradeC (+50.0 meese/sec)</button>
+  <div class="label-text">cost: <span id="upgradeCostC">1000</span> doubloons</div>
+  <div class="label-text">purchased: <span id="purchasedB">0</span> units</div>
 `;
 
 // click handler
 const button = document.getElementById("moots")!;
 const counterElement = document.getElementById("counter")!;
-const buyButton = document.getElementById("buyButton")!;
-const costElement = document.getElementById("upgradeCost")!;
+
+// buy buttons
+const buyButtonA = document.getElementById("buyButtonA")!;
+const buyButtonB = document.getElementById("buyButtonB")!;
+const buyButtonC = document.getElementById("buyButtonC")!;
+
+// buy buttons
+const purchasedAElement = document.getElementById("purchasedA")!;
+const purchasedBElement = document.getElementById("purchasedB")!;
+const purchasedCElement = document.getElementById("purchasedC")!;
+
+// associated upgrade costs
+//const costElementA = document.getElementById("upgradeCostA")!;
+//const costElementB = document.getElementById("upgradeCostB")!;
+//const costElementC = document.getElementById("upgradeCostC")!;
+
+const growthElement = document.getElementById("growthRate")!;
 
 button.addEventListener("click", () => {
   click();
 });
-buyButton.addEventListener("click", () => {
-  buyUpgrade();
+buyButtonA.addEventListener("click", () => {
+  buyUpgrade(costA, upsA);
+  purchasedA++;
+  purchasedAElement.innerHTML = `${purchasedA}`;
+});
+buyButtonB.addEventListener("click", () => {
+  buyUpgrade(costB, upsB);
+  purchasedB++;
+  purchasedBElement.innerHTML = `${purchasedB}`;
+});
+buyButtonC.addEventListener("click", () => {
+  buyUpgrade(costC, upsC);
+  purchasedC++;
+  purchasedCElement.innerHTML = `${purchasedC}`;
 });
 
 // FUNCTION CALLS
@@ -37,7 +84,9 @@ requestAnimationFrame(step);
 function click(amount: number = 1) {
   counter += amount;
   counterElement.innerHTML = `${Math.trunc(counter)}`;
-  checkUpgrade();
+  checkUpgrade(costA, buyButtonA);
+  checkUpgrade(costB, buyButtonB);
+  checkUpgrade(costC, buyButtonC);
 }
 
 function step(timestamp: number) {
@@ -52,18 +101,21 @@ function step(timestamp: number) {
   requestAnimationFrame(step);
 }
 
-function checkUpgrade() {
-  if (counter >= upgradeCost) {
-    buyButton.removeAttribute("disabled");
-  } else if (!buyButton.hasAttribute("disabled")) {
-    buyButton.setAttribute("disabled", "true");
+function checkUpgrade(cost: number, buttonElement: HTMLElement) {
+  if (counter >= cost) {
+    buttonElement.removeAttribute("disabled");
+  } else if (!buttonElement.hasAttribute("disabled")) {
+    buttonElement.setAttribute("disabled", "true");
   }
 }
 
-function buyUpgrade() {
-  counter -= upgradeCost;
-  upgradeCost *= 2;
-  autoClicks++;
-  costElement.innerHTML = `${upgradeCost}`;
+function buyUpgrade(
+  cost: number,
+  unitsPerSec: number,
+) {
+  counter -= cost;
+  autoClicks += unitsPerSec;
+  //costElement.innerHTML = `${upgradeCost}`;
   counterElement.innerHTML = `${Math.trunc(counter)}`;
+  growthElement.innerHTML = autoClicks.toFixed(1);
 }
